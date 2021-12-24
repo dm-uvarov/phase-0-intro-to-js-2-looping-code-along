2b3c09ad8981121c479467620ab789e0138e253e
Dmitry Uvarov <dm.a.uvarov@gmail.com>
2021-12-23 23:06:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+// ================== function writeCards
+function writeCards(arrStr,eventName) {
+
+    let arrMsgs = [];
+    debugger;
+    for (let i = 0; i < arrStr.length; i++ ) {
+        debugger;
+        arrMsgs.push(`Thank you, ${arrStr[i]}, for the wonderful ${eventName} gift!`);
+        debugger;
+    } //end for loop
+    
+    return arrMsgs;
+} // ================ end of function writeCards
+
+// ================== function countDown 
+function countDown(positiveNumber){
+    if (positiveNumber < 0){
+        return "not a positive number"
+    }else{
+        do {
+            console.log(positiveNumber);
+            positiveNumber-- ;
+        } while (positiveNumber >= 0);
+        // end loop of do ... while
+    } //end if else
+}// ================ end of function countDown
